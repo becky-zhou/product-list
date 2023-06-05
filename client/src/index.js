c47c1afda17bc54897fd5f7634f67becf62b6235
Becky <tug76312@temple.edu>
2023-06-04 21:22:10 -0400

    broken react-app

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'; 
+import { Provider, createStoreHook } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'; 
+import ReduxPromise from 'redux-promise'; 
+import 'react-blocks/dist/styles.css';
+
+
+import reducers from './reducers'
+
+const createStoreWithMiddleware =applyMiddleware(ReduxPromise)(createStore); 
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={createStoreWithMiddleware(reducers)}>
     <App/>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
---
+++ client/src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+import ProductReducer from './reducer-product'
+
+
+
+const rootReducer = combineReducers({
+    
+    product : ProductReducer
+}); 
+
+export default rootReducer; 
